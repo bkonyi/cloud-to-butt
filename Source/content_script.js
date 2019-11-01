3a31ca9f0c1647173d5f4a66d64ef8d879547f15
Ben Konyi <bkonyi@google.com>
2019-10-31 19:02:34 -0700

    Florida man

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bDonald Trump\b/g, "Florida Man");
+	v = v.replace(/\bTrump\b/g, "Florida Man");
+	v = v.replace(/\bTRUMP\b/g, "FLORIDA MAN");
 	
 	textNode.nodeValue = v;
 }
